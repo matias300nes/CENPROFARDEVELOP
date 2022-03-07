b606be08e603573ab62ac22367e7504c26bed4e6
matias300nes <matiendres@gmail.com>
2022-03-07 09:37:18 -0300

    arreglo en eleccion de periodo

--- frmPresentaciones.vb
+++ frmPresentaciones.vb
@@ -2493,7 +2493,10 @@ Public Class frmPresentaciones
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        txtPeriodo.Text = $"{LbPeriodo_parte.Text} {LbPeriodo_Mes.Text}-{LbPeriodo_año.Text}"
+        txtPeriodo.Text = IIf(LbPeriodo_parte.Text <> "MENSUAL",
+                                $"{LbPeriodo_parte.Text} {LbPeriodo_Mes.Text}-{LbPeriodo_año.Text}",
+                                $"{LbPeriodo_Mes.Text}-{LbPeriodo_año.Text}"
+                                )
         GbPeriodo.Visible = Not GbPeriodo.Visible
     End Sub
 
