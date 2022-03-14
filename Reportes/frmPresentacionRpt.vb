16d19e6a3af6c0ab6f06429e4e90dbde6b6ae513
ignavila13 <nachoavil13@gmail.com>
2022-03-14 10:23:35 -0300

    Fin reporte

--- Reportes/frmPresentacionRpt.vb
+++ Reportes/frmPresentacionRpt.vb
@@ -11,10 +11,16 @@
         Me.idpresentacion = idpresentacion
 
     End Sub
+
+
     Private Sub frmPresentacionRpt_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'TODO: esta línea de código carga datos en la tabla 'dsSistema.spRPT_Presentacion' Puede moverla o quitarla según sea necesario.
         Me.spRPT_PresentacionTableAdapter.Fill(Me.dsSistema.spRPT_Presentacion, idpresentacion)
-
+        Dim ObraSocial = dsSistema.Tables(0).Rows(0).Item(0)
+        Dim Periodo = dsSistema.Tables(0).Rows(0).Item(3)
+        Dim FechaActual = Today.Date.ToString("dd/MM/yyyy")
+        FechaActual = Replace(FechaActual, "/", "-")
+        Me.ReportViewer1.LocalReport.DisplayName = "PRESENTACIÓN " + ObraSocial + "-" + Periodo
         Me.ReportViewer1.RefreshReport()
     End Sub
 End Class
