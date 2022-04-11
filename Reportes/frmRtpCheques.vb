972bdec886ea6f0fb933b93cc457329f4ddfc989
matias300nes <matiendres@gmail.com>
2022-04-11 17:06:49 -0300

    report cheques. estructura

--- Reportes/frmRtpCheques.vb
+++ Reportes/frmRtpCheques.vb
@@ -8,25 +8,35 @@
         Monto = 5
         IdFarmacia = 6
     End Enum
+    Dim Cheques As DataTable
     Public Sub New(Cheques As DataTable)
 
         ' Esta llamada es exigida por el diseñador.
         InitializeComponent()
 
         ' Agregue cualquier inicialización después de la llamada a InitializeComponent().
-        For Each cheque As DataRow In Cheques.Rows
-            Dim row As dsSistema.PagosRow = dsSistema.Pagos.NewPagosRow
-            row.PagueseA = cheque(colsCheques.PagueseA)
-            row.Monto = cheque(colsCheques.Monto)
-            row.FechaPago = cheque(colsCheques.FechaPago)
-            row.FechaEmision = DateTime.Today
-        Next
+        Me.Cheques = Cheques
     End Sub
     Private Sub frmRtpCheques_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'TODO: esta línea de código carga datos en la tabla 'dsSistema.spRPT_Presentacion' Puede moverla o quitarla según sea necesario.
         'Me.spRPT_PresentacionTableAdapter.Fill(Me.dsSistema.spRPT_Presentacion)
 
+        Dim i As Integer = 0
+        For Each cheque As DataRow In Cheques.Rows
+            Dim row As dsSistema.PagosRow = dsSistema.Pagos.NewPagosRow
+            row.Id = i
+            row.PagueseA = cheque(colsCheques.PagueseA)
+            row.Monto = cheque(colsCheques.Monto)
+            row.FechaPago = cheque(colsCheques.FechaPago)
+            row.FechaEmision = DateTime.Today
+            dsSistema.Pagos.AddPagosRow(row)
+            i += 1
+        Next
+        MsgBox(dsSistema.Pagos.Rows.Count)
+        'var reportDataSource1 = New ReportDataSource("NameOfReportDataSet", YourDataTable);
+        'this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+
         Me.ReportViewer1.RefreshReport()
-        Me.ReportViewer1.RefreshReport()
+        'Me.ReportViewer1.RefreshReport()
     End Sub
 End Class
